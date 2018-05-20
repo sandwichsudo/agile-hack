f52073daba631bee99c059457d5e0e214140d1cc
Gilly Ames <gilly.ames@octopus.energy>
2018-05-20 21:54:19 +0100

    Fix api url

--- client/src/xhr/xhr.js
+++ client/src/xhr/xhr.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: '',
   headers: { 'Content-Type': 'application/json;charset=UTF-8' },
 });
 
